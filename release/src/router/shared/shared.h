5d82f49963b23da8d6bfbc5cde5d0989fbe4c59d
pedro <pedro311@gmail.com>
2019-10-12 12:47:08 +0200

    router: rc/shared: fix compiler warnings (align to ARM branch)

--- release/src/router/shared/shared.h
+++ release/src/router/shared/shared.h
@@ -327,7 +327,7 @@ static inline int led(int which, int mode)
 
 
 /* base64.c */
-extern int base64_encode(unsigned char *in, char *out, int inlen);		/* returns amount of out buffer used */
+extern int base64_encode(const char *in, char *out, int inlen);			/* returns amount of out buffer used */
 extern int base64_decode(const char *in, unsigned char *out, int inlen);	/* returns amount of out buffer used */
 extern int base64_encoded_len(int len);
 extern int base64_decoded_len(int len);						/* maximum possible, not actual */
