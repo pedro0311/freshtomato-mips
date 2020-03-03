77ef56a514788af7515adf39929cc5a9520af9d3
M_ars <M_ars@gmx.net>
2020-03-03 19:14:24 +0100

    rc: init.c - remove start_nas()/stop_nas() (already done at start_services()/stop_services())
    And important: call stop_nas() first before we start again

--- release/src/router/rc/init.c
+++ release/src/router/rc/init.c
@@ -1632,7 +1632,6 @@ int init_main(int argc, char *argv[])
 			run_nvscript("script_shut", NULL, 10);
 
 			stop_services();
-			stop_nas();
 			stop_wan();
 			stop_arpbind();
 			stop_lan();
@@ -1684,7 +1683,6 @@ int init_main(int argc, char *argv[])
 			start_vlan();
 			start_lan();
 			start_arpbind();
-			start_nas();
 			mwan_state_files();
 			start_services();
 
