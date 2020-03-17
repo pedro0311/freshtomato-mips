e4c1173222079f3e3a2948abf649ef3c536897ef
pedro <pedro311@gmail.com>
2020-03-17 20:17:01 +0100

    router: rc: services.c: simplify if statement

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -3272,17 +3272,7 @@ TOP:
 		goto CLEAR;
 	}
 
-	if (strcmp(service, "wireless") == 0) {
-		if (act_stop) {
-			stop_wireless();
-		}
-		if (act_start) {
-			restart_wireless();
-		}
-		goto CLEAR;
-	}
-
-	if (strcmp(service, "wl") == 0) {
+	if ((strcmp(service, "wireless") == 0) || (strcmp(service, "wl") == 0)) {
 		if (act_stop) {
 			stop_wireless();
 		}
