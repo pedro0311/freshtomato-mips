f4c21c11f79c74b472104ebd1a11b9ead9a7c9bf
Tvlz <Tvlz@Tvlz>
2020-07-28 11:22:54 -0400

    Change Netgear Model Detection for WNDR3400/v2/v3 and WNDR3700v3/4000
    Taken from Arrmo sources

--- release/src/router/shared/id.c
+++ release/src/router/shared/id.c
@@ -249,6 +249,49 @@ int getMTD(char *name)
 	return device;
 }
 
+int get_wndr_model(void)
+{
+	#define	WNDR4000		"U12H181T00_NETGEAR"
+	#define WNDR3700v3		"U12H194T00_NETGEAR"
+	#define WNDR3400		"U12H155T00_NETGEAR"
+	#define	WNDR3400v2		"U12H187T00_NETGEAR"
+	#define WNDR3400v3		"U12H208T00_NETGEAR"
+
+	int mtd = getMTD("board_data");
+	char devname[32];
+	sprintf(devname, "/dev/mtd%dro", mtd);
+	FILE *model = fopen(devname, "rb");
+
+	if (model) {
+		char modelstr[32];
+		fread(modelstr, 1, 32, model);
+		nvram_set("board_id", modelstr);
+		fclose(model);
+		if (strcmp(modelstr, WNDR4000) == 0) {
+			nvram_set("netgear_model", "wndr4000");
+			return MODEL_WNDR4000;
+		}
+		if (strcmp(modelstr, WNDR3700v3) == 0) {
+			nvram_set("netgear_model", "wndr3700v3");
+			return MODEL_WNDR3700v3;
+		}
+		if (strcmp(modelstr, WNDR3400) == 0) {
+			nvram_set("netgear_model", "wndr3400");
+			return MODEL_WNDR3400;
+		}
+		if (strcmp(modelstr, WNDR3400v2) == 0) {
+			nvram_set("netgear_model", "wndr3400v2");
+			return MODEL_WNDR3400v2;
+		}
+		if (strcmp(modelstr, WNDR3400v3) == 0) {
+			nvram_set("netgear_model", "wndr3400v3");
+			return MODEL_WNDR3400v3;
+		}
+	}
+
+	return MODEL_UNKNOWN;
+}
+
 int get_model(void)
 {
 	int hw;
@@ -360,44 +403,15 @@ int get_model(void)
 			return MODEL_WRT610Nv2;
 		if (nvram_match("boot_hw_model", "E4200"))
 			return MODEL_E4200;
+
 		if (nvram_match("boardmfg", "NETGEAR")) {
-			// Netgear Router ... so check board_data partition information (as at least WNDR4000 and WNDR3700v3 need this to identify)
-			// Note that boardmfg is stored in CFE - so always present!
-			int mtd = getMTD("board_data");
-			char devname[32];
-			sprintf(devname, "/dev/mtd%dro", mtd);
-			FILE *model = fopen(devname, "rb");
-			if (model) {
-				#define	WNDR4000		"U12H181T00_NETGEAR"
-				#define WNDR3700v3		"U12H194T00_NETGEAR"
-				#define WNDR3400		"U12H155T00_NETGEAR"
-				#define	WNDR3400v2		"U12H187T00_NETGEAR"
-				#define WNDR3400v3		"U12H208T00_NETGEAR"
-				char modelstr[32];
-				// Get Model string from board_data partition, null terminated in NVRAM already (checked using dd dump)
-				fread(modelstr, 1, 32, model);
-				nvram_set("board_id", modelstr);
-				fclose(model);
-				// Determine / return router model (based on Model string)
-				if (!strcmp(modelstr, WNDR3700v3)) {
-					//syslog(LOG_INFO, "NETGEAR: Model detected based on board_data model string, result = wndr3700v3\n");
-					nvram_set("netgear_model", "wndr3700v3");
-					return MODEL_WNDR3700v3;
-				}
-				if (!strcmp(modelstr, WNDR3400)) {
-					//syslog(LOG_INFO, "NETGEAR: Model detected based on board_data model string, result = wndr3400\n");
-					nvram_set("netgear_model", "wndr3400");
-					return MODEL_WNDR3400;
-				} 
-				//syslog(LOG_INFO, "NETGEAR: Model detected based on board_data model string, result = wndr4000\n");
-				nvram_set("netgear_model", "wndr4000");
-				return MODEL_WNDR4000;
-			} else {
-				// Can't open board_data partition, so default to WNDR4000 ... but log to syslog
+			int wndr_model = get_wndr_model();
+			if (wndr_model == MODEL_UNKNOWN) {
+				wndr_model = MODEL_WNDR4000;
 				syslog(LOG_INFO, "NETGEAR: Cannot open board_data partition to check model string, defaulting to WNDR4000 ... but this may not be correct!\n");
 				nvram_set("netgear_model", "wndr4000");
-				return MODEL_WNDR4000;
 			}
+			return wndr_model;
 		}
 		
 		switch (strtoul(nvram_safe_get("boardtype"), NULL, 0)) {
@@ -430,6 +444,7 @@ int get_model(void)
 		}
 	}
 #endif
+
 	switch (strtoul(nvram_safe_get("boardnum"), NULL, 0)) {
 	case 42:
 		switch (hw) {
@@ -583,25 +598,13 @@ int get_model(void)
 			break;
 		case HW_BCM5358U:
 			if (nvram_match("boardrev", "0x1400")) {
-				// WNDR3400v2 & WNDR3400v3 has same boardrev
-				int mtd = getMTD("board_data");
-				char devname[32];
-				sprintf(devname, "/dev/mtd%dro", mtd);
-				FILE *model = fopen(devname, "rb");
-				if (model) {
-					#define WNDR3400v3		"U12H208T00_NETGEAR"
-					char modelstr[32];
-					// Get Model string from board_data partition, null terminated in NVRAM already (checked using dd dump)
-					fread(modelstr, 1, 32, model);
-					fclose(model);
-					if (!strcmp(modelstr, WNDR3400v3)) {
-						nvram_set("netgear_model", "wndr3400v3");
-						return MODEL_WNDR3400v3;
-					}
+				int wndr_model = get_wndr_model();
+				if (wndr_model == MODEL_UNKNOWN) {
+					wndr_model = MODEL_WNDR3400v2;
+					syslog(LOG_INFO, "NETGEAR: Cannot open board_data partition to check model string, defaulting to WNDR3400v2 ... but this may not be correct!\n");
+					nvram_set("netgear_model", "wndr3400v2");
 				}
-				// default to WNDR3400v2
-				nvram_set("netgear_model", "wndr3400v2");
-				return MODEL_WNDR3400v2;
+				return wndr_model;
 			}
 			break;
 		}
