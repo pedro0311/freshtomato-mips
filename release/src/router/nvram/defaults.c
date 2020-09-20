d0f9029453c08134a00cb06ee89176169aa0d592
M_ars <M_ars@gmx.net>
2020-09-20 21:48:52 +0200

    nvram: defaults.c: adjust redial period to 20 seconds
    Note: this (minimum) waiting time helps with dual-stack to get a fresh IPv6 setup.
    
    tested and confirmed :)
    
    (see also arm issue 13)

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -272,7 +272,7 @@ const defaults_t defaults[] = {
 	{ "wan_ppp_idletime",		"5"				},	// Dial on demand max idle time (mins)
 	{ "wan_ppp_demand",		"0"				},	// Dial on demand
 	{ "wan_ppp_demand_dnsip",	"198.51.100.1"			},	// IP to which DNS queries are sent to trigger Connect On Demand
-	{ "wan_ppp_redialperiod",	"10"				},	// Redial Period  (seconds)
+	{ "wan_ppp_redialperiod",	"20"				},	// Redial Period  (seconds)
 	{ "wan_ppp_service",		""				},	// PPPoE service name
 	{ "wan_ppp_custom",		""				},	// PPPD additional options
 	{ "wan_ppp_mlppp",		"0"				},	// PPPoE single line MLPPP
@@ -284,7 +284,7 @@ const defaults_t defaults[] = {
 	{ "wan2_ppp_idletime",		"5"				},	// Dial on demand max idle time (mins)
 	{ "wan2_ppp_demand",		"0"				},	// Dial on demand
 	{ "wan2_ppp_demand_dnsip",	"198.51.100.1"			},	// IP to which DNS queries are sent to trigger Connect On Demand
-	{ "wan2_ppp_redialperiod",	"10"				},	// Redial Period  (seconds)
+	{ "wan2_ppp_redialperiod",	"20"				},	// Redial Period  (seconds)
 	{ "wan2_ppp_service",		""				},	// PPPoE service name
 	{ "wan2_ppp_custom",		""				},	// PPPD additional options
 	{ "wan2_ppp_mlppp",		"0"				},	// PPPoE single line MLPPP
@@ -297,7 +297,7 @@ const defaults_t defaults[] = {
 	{ "wan3_ppp_idletime",		"5"				},	// Dial on demand max idle time (mins)
 	{ "wan3_ppp_demand",		"0"				},	// Dial on demand
 	{ "wan3_ppp_demand_dnsip",	"198.51.100.1"			},	// IP to which DNS queries are sent to trigger Connect On Demand
-	{ "wan3_ppp_redialperiod",	"10"				},	// Redial Period  (seconds)
+	{ "wan3_ppp_redialperiod",	"20"				},	// Redial Period  (seconds)
 	{ "wan3_ppp_service",		""				},	// PPPoE service name
 	{ "wan3_ppp_custom",		""				},	// PPPD additional options
 	{ "wan3_ppp_mlppp",		"0"				},	// PPPoE single line MLPPP
@@ -309,7 +309,7 @@ const defaults_t defaults[] = {
 	{ "wan4_ppp_idletime",		"5"				},	// Dial on demand max idle time (mins)
 	{ "wan4_ppp_demand",		"0"				},	// Dial on demand
 	{ "wan4_ppp_demand_dnsip",	"198.51.100.1"			},	// IP to which DNS queries are sent to trigger Connect On Demand
-	{ "wan4_ppp_redialperiod",	"10"				},	// Redial Period  (seconds)
+	{ "wan4_ppp_redialperiod",	"20"				},	// Redial Period  (seconds)
 	{ "wan4_ppp_service",		""				},	// PPPoE service name
 	{ "wan4_ppp_custom",		""				},	// PPPD additional options
 	{ "wan4_ppp_mlppp",		"0"				},	// PPPoE single line MLPPP
