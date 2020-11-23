0532d4f3b1c9b08ec4682db546b4362f2137ecd7
pedro <pedro311@gmail.com>
2020-11-23 20:22:45 +0100

    rc: firewall.c: be more restrictive, only allow ICMP messages we need
    - thanks @rs232

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -629,7 +629,7 @@ const defaults_t defaults[] = {
 	{ "nf_loopback",		"0"				},
 	{ "block_wan",			"1"				},	// block inbound icmp
 	{ "block_wan_limit",		"1"				},
-	{ "block_wan_limit_icmp",	"1"				},
+	{ "block_wan_limit_icmp",	"3"				},
 	{ "multicast_pass",		"0"				},	// enable multicast proxy
 	{ "multicast_lan",		"0"				},	// on LAN (br0)
 	{ "multicast_lan1",		"0"				},	// on LAN1 (br1)
