80e4f1929d399df9b6647a2b0d86408bd7441926
pedro <pedro311@gmail.com>
2021-01-04 12:31:01 +0100

    stubby: add full GUI support (part 1/2)
    - based on @RMerlin work (thanks!)

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -163,8 +163,10 @@ const defaults_t defaults[] = {
 #endif
 #ifdef TCONFIG_STUBBY
 	{ "stubby_proxy",		"0"				},
-	{ "stubby_priority",		"2"				},	// 0=none, 1=strict-order, 2=no-resolv
-	{ "stubby_log",			"4"				},	// log level
+	{ "stubby_priority",		"2"				},	/* 0=none, 1=strict-order, 2=no-resolv */
+	{ "stubby_port",		"5453"				},	/* local port */
+	{ "stubby_resolvers",		"<1.1.1.1>>cloudflare-dns.com><1.0.0.1>>cloudflare-dns.com>"},	/* default DoT resolvers */
+	{ "stubby_log",			"4"				},	/* log level */
 #endif
 	{ "wan_wins",			""				},	// x.x.x.x x.x.x.x ...
 	{ "wan_lease",			"86400"				},	// WAN lease time in seconds
