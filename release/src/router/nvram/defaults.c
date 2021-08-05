542a780803b1e91357f03821a6fe96fefcfb5524
M_ars <M_ars@gmx.net>
2021-08-05 12:03:00 +0200

    Wireless Client Mode: repair that operation mode for SDK6 and up
    Note: Client Mode was already working for mips branch SDK5. Only one wl client is possible! (no change)
    
    tested with arm dual core, working correct! :)
    
    Note 2: sync to arm branch and add some more things for mips SDK6

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -399,7 +399,7 @@ const defaults_t defaults[] = {
 	{ "wl_btc_mode",		"0"				},	// !!TB - BT Coexistence Mode
 	{ "wl_sta_retry_time",		"5"				},	// !!TB - Seconds between association attempts (0 to disable retries)
 	{ "wl_mitigation",		"0"				},	// Non-AC Interference Mitigation Mode (0|1|2|3|4)
-#ifdef TCONFIG_BCMWL6
+#ifdef TCONFIG_BCMARM
 	{ "wl_mitigation_ac",		"0"				},	// AC Interference Mitigation Mode (bit mask (3 bits), values from 0 to 7); 0 == disabled
 #endif
 	{ "wl_passphrase",		""				},	// Passphrase
