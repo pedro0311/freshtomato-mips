2fb275ec92e58eb552742ce22dbbb0be68b60082
pedro <pedro311@gmail.com>
2021-11-05 02:02:41 +0100

    dhcpv6: remove debug info - save some space; use getifaddrs.c from ARM branch (it's not needed btw)

--- release/src/router/dhcpv6/debug.h
+++ release/src/router/dhcpv6/debug.h
@@ -2,14 +2,22 @@
 #define MY_DEBUG_H_
 
 #ifndef MAX_DEBUG_LEVEL
-# define MAX_DEBUG_LEVEL	LOG_NOTICE
+# define MAX_DEBUG_LEVEL	LOG_DEBUG
 #endif
 
 /* First include stdio.h, which may contain the prototype for the external dprintf.
  * We do not want that. We redefine dprintf to our local implementation. */
 #include <stdio.h>
+
+#ifdef NEED_DEBUG
+
 #define dprintf(level, args...) \
 	( { if ((level) <= MAX_DEBUG_LEVEL) my_dprintf((level), args); } )
+
+
+#else
+#define dprintf(level, args...) do {} while(0)
+#endif
 
 #ifndef __P
 # define __P(x) x
