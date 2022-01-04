d31f59c0216e593ea394f551f0b2710c3b557f76
pedro <pedro311@gmail.com>
2022-01-04 23:49:15 +0100

    router: others: one version for ARM and MIPS branch; clean-up

--- release/src/router/others/entware-install-MIPS.sh
+++ release/src/router/others/entware-install-MIPS.sh
@@ -13,29 +13,27 @@ WGET="/usr/bin/wget"
 URL=http://pkg.entware.net/binaries/mipsel/installer
 
 echo "Info: Creating folders..."
-for folder in bin etc/init.d lib/opkg sbin share tmp usr var/log var/lock var/run
-do
-  if [ -d "/opt/$folder" ]
-  then
-    echo "Warning: Folder /opt/$folder exists! If something goes wrong please clean /opt folder and try again."
-  else
-    mkdir -p /opt/$folder
-  fi
+for folder in bin etc/init.d lib/opkg sbin share tmp usr var/log var/lock var/run; do
+	[ -d "/opt/$folder" ] && {
+		echo "Warning: Folder /opt/$folder exists! If something goes wrong please clean /opt folder and try again."
+	} || {
+		mkdir -p /opt/$folder
+	}
 done
 
 dl () {
-  # $1 - URL to download
-  # $2 - place to store
-  # $3 - 'x' if should be executable
-  echo -n "Downloading $2... "
-  $WGET -q $1 -O $2
-  if [ $? -eq 0 ] ; then
-    echo "success!"
-  else
-    echo "failed!"
-    exit 1
-  fi
-  [ -z "$3" ] || chmod +x $2
+	# $1 - URL to download
+	# $2 - place to store
+	# $3 - 'x' if should be executable
+	echo -n "Downloading $2... "
+	$WGET -q $1 -O $2
+	[ $? -eq 0 ] && {
+		echo "success!"
+	} || {
+		echo "failed!"
+		exit 1
+	}
+	[ -z "$3" ] || chmod +x $2
 }
 
 echo "Info: Deploying opkg package manager..."
