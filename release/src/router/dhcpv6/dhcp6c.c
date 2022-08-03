c92a6e9e862a037f1fb518d077dbd3e3805c3f4b
pedro <pedro311@gmail.com>
2022-08-03 18:32:05 +0200

    build: dhcpv6: add #ifdef to have one version for ARM and MIPS

--- release/src/router/dhcpv6/dhcp6c.c
+++ release/src/router/dhcpv6/dhcp6c.c
@@ -311,6 +311,12 @@ client6_init()
 		    gai_strerror(error));
 		exit(1);
 	}
+#ifdef __linux__
+#ifdef HAVE_CLOEXEC
+	/* Force socket to be closed on execve */
+	res->ai_socktype |= SOCK_CLOEXEC;
+#endif
+#endif
 	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	if (sock < 0) {
 		dprintf(LOG_ERR, FNAME, "socket");
