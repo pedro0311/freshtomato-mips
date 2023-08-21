7f74c50987beb1fc5ea917c3156f1ab2007ca4da
pedro <pedro311@gmail.com>
2023-08-21 12:53:07 +0200

    nvram: defaults.c: lower mwan_diff value (again) to avoid false positives

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -142,7 +142,7 @@ const defaults_t defaults[] = {
 	{ "mwan_debug",			"0"				},
 	{ "mwan_tune_gc",		"0"				},	/* tune route cache for multiwan in load balancing */
 	{ "mwan_state_init",		"1"				},	/* init wan state files with this value */
-	{ "mwan_diff",			"400"				},	/* declare the minimum number of bytes indicating a working WAN (only for tracert) */
+	{ "mwan_diff",			"200"				},	/* declare the minimum number of bytes indicating a working WAN (only for tracert) */
 	{ "pbr_rules",			""				},
 
 	/* WAN H/W parameters */
