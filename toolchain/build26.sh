1376fe9f814300eb0ed0c015dd42ea8956be4dff
pedro <pedro311@gmail.com>
2024-03-11 23:42:25 +0100

    toolchain: correct build script and replace ctype.h file with correct one after building toolchain

--- toolchain/build26.sh
+++ toolchain/build26.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 
 #########################################################################
 #                         Toolchain Build Script                        #
@@ -9,6 +9,7 @@ GCCVER=4.2.4
 ROOTDIR=$PWD
 TARGETDIR=hndtools-mipsel-uclibc-${GCCVER}
 DESTDIR=../tools/brcm/${TARGETDIR}
+FILES=./dl/files
 
 make -C ../release/src-rt prepk
 
@@ -20,22 +21,31 @@ cd $ROOTDIR
 
 rm -f .config
 ln -sf config.2.6-${GCCVER} .config
-make clean; make dirclean; make V=99
+make clean
+make dirclean
+make V=99
 
-cd $DESTDIR/bin
-ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-linux-uclibc-gcc
-ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-linux-gcc-${GCCVER}
-ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-uclibc-gcc-${GCCVER}
+[ -d "$DESTDIR/bin" ] && {
+	cd $ROOTDIR
+	cp -f $FILES/ctype.h $DESTDIR/include/ctype.h
 
-cd ../..
-rm -f hndtools-mipsel-linux
-rm -f hndtools-mipsel-uclibc
+	cd $DESTDIR/bin
+	ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-linux-uclibc-gcc
+	ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-linux-gcc-${GCCVER}
+	ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-uclibc-gcc-${GCCVER}
 
-mkdir -p K26
-rm -rf K26/hndtools-mipsel-uclibc-${GCCVER}
-mv -f hndtools-mipsel-uclibc-${GCCVER} K26/
+	cd ../..
+	rm -f hndtools-mipsel-linux
+	rm -f hndtools-mipsel-uclibc
 
-ln -nsf K26/hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-linux
-ln -nsf K26/hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-uclibc
+	mkdir -p K26
+	rm -rf K26/hndtools-mipsel-uclibc-${GCCVER}
+	mv -f hndtools-mipsel-uclibc-${GCCVER} K26/
 
-cd $ROOTDIR
+	ln -nsf K26/hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-linux
+	ln -nsf K26/hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-uclibc
+
+	cd $ROOTDIR
+
+	echo -e "\nToolchain successfully built!\n\n"
+}
