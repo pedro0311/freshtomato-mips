3c1baafcb927ed1a3e9b5e5eb8112c872f5371fa
pedro <pedro311@gmail.com>
2025-09-05 13:17:55 +0200

    rc: introduce and use restart_firewall() function. Move restart_firewall() to the end in exec_service()

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -287,6 +287,12 @@ void del_bsd_defaults(void)
 }
 #endif /* TCONFIG_BCMBSD */
 
+void restart_firewall(void)
+{
+	stop_firewall();
+	start_firewall();
+}
+
 #ifdef TCONFIG_DNSCRYPT
 void start_dnscrypt(void)
 {
@@ -2620,14 +2626,13 @@ TOP:
 			stop_nocat();
 #endif
 		}
-		stop_firewall();
-		start_firewall(); /* always restarted */
 		if (act_start) {
 			start_bwlimit();
 #ifdef TCONFIG_NOCAT
 			start_nocat();
 #endif
 		}
+		restart_firewall(); /* always restart */
 		goto CLEAR;
 	}
 
@@ -2639,8 +2644,6 @@ TOP:
 				stop_qos(buffer2);
 			}
 		}
-		stop_firewall();
-		start_firewall(); /* always restarted */
 		if (act_start) {
 			for (i = 1; i <= MWAN_MAX; i++) {
 				memset(buffer2, 0, sizeof(buffer2));
@@ -2651,14 +2654,14 @@ TOP:
 			if (nvram_get_int("qos_reset"))
 				f_write_string("/proc/net/clear_marks", "1", 0, 0);
 		}
+		restart_firewall(); /* always restart */
 		goto CLEAR;
 	}
 
 	if ((strcmp(service, "upnp") == 0) || (strcmp(service, "miniupnpd") == 0)) {
 		if (act_stop) stop_upnp();
-		stop_firewall();
-		start_firewall(); /* always restarted */
 		if (act_start) start_upnp();
+		restart_firewall(); /* always restart */
 		goto CLEAR;
 	}
 
@@ -2695,8 +2698,6 @@ TOP:
 			stop_telnetd();
 			stop_httpd();
 		}
-		stop_firewall();
-		start_firewall(); /* always restarted */
 		if (act_start) {
 			stop_httpd();
 			start_httpd();
@@ -2707,6 +2708,7 @@ TOP:
 			if (nvram_get_int("sshd_eas") && (!(strcmp(service, "adminnosshd") == 0)))
 				start_sshd();
 		}
+		restart_firewall(); /* always restart */
 		goto CLEAR;
 	}
 
@@ -2729,8 +2731,7 @@ TOP:
 			/* always restarted except from "service" command */
 			stop_cron();
 			start_cron();
-			stop_firewall();
-			start_firewall();
+			restart_firewall();
 		}
 		goto CLEAR;
 	}
@@ -2862,8 +2863,6 @@ TOP:
 					eval("brctl", "stp", nvram_safe_get(buffer2), "0");
 			}
 		}
-		stop_firewall();
-		start_firewall();
 		if (act_start) {
 			do_static_routes(1); /* add new */
 #ifdef TCONFIG_ZEBRA
@@ -2879,14 +2878,14 @@ TOP:
 				}
 			}
 		}
+		restart_firewall(); /* always restart */
 		goto CLEAR;
 	}
 
 	if (strcmp(service, "ctnf") == 0) {
 		if (act_start) {
 			setup_conntrack();
-			stop_firewall();
-			start_firewall();
+			restart_firewall(); /* always restart */
 		}
 		goto CLEAR;
 	}
@@ -3053,9 +3052,8 @@ TOP:
 #ifdef TCONFIG_TOR
 	if (strcmp(service, "tor") == 0) {
 		if (act_stop) stop_tor();
-		stop_firewall();
-		start_firewall(); /* always restarted */
 		if (act_start) start_tor(1); /* force (re)start */
+		restart_firewall(); /* always restart */
 		goto CLEAR;
 	}
 #endif
