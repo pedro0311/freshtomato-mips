4069408328dc92dd340a3bb74b0aee2609b141c2
pedro <pedro311@gmail.com>
2025-10-09 17:03:41 +0200

    rc: firewall.c: increase hitcount limit for remote GUI access

--- release/src/router/rc/firewall.c
+++ release/src/router/rc/firewall.c
@@ -989,7 +989,7 @@ static void filter_input(void)
 
 		ipt_write("-N wwwlimit\n"
 		          "-A wwwlimit -m recent --set --name www\n"
-		          "-A wwwlimit -m recent --update --hitcount 15 --seconds 5 --name www -j %s\n",
+		          "-A wwwlimit -m recent --update --hitcount 30 --seconds 5 --name www -j %s\n",
 		          chain_in_drop);
 
 		if (nvram_get_int("dmz_enable") && nvram_get_int("dmz_ra"))
@@ -1476,7 +1476,7 @@ static void filter6_input(void)
 
 		ip6t_write("-N wwwlimit\n"
 		           "-A wwwlimit -m recent --set --name www\n"
-		           "-A wwwlimit -m recent --update --hitcount 15 --seconds 5 --name www -j %s\n",
+		           "-A wwwlimit -m recent --update --hitcount 30 --seconds 5 --name www -j %s\n",
 		           chain_in_drop);
 
 		if (nvram_get_int("dmz_enable") && nvram_get_int("dmz_ra"))
