20048c888c9e1882007243aebe6e4b98539e9e8f
pedro <pedro311@gmail.com>
2025-10-26 00:57:48 +0200

    rc: firewall.c: reduce hitcount for www to 20 - MIPS doesn't allow higher values

--- release/src/router/rc/firewall.c
+++ release/src/router/rc/firewall.c
@@ -984,7 +984,7 @@ static void filter_input(void)
 
 		ipt_write("-N wwwlimit\n"
 		          "-A wwwlimit -m recent --set --name www\n"
-		          "-A wwwlimit -m recent --update --hitcount 30 --seconds 5 --name www -j %s\n",
+		          "-A wwwlimit -m recent --update --hitcount 20 --seconds 3 --name www -j %s\n",
 		          chain_in_drop);
 
 		if (nvram_get_int("dmz_enable") && nvram_get_int("dmz_ra"))
@@ -1479,7 +1479,7 @@ static void filter6_input(void)
 
 		ip6t_write("-N wwwlimit\n"
 		           "-A wwwlimit -m recent --set --name www\n"
-		           "-A wwwlimit -m recent --update --hitcount 30 --seconds 5 --name www -j %s\n",
+		           "-A wwwlimit -m recent --update --hitcount 20 --seconds 3 --name www -j %s\n",
 		           chain_in_drop);
 
 		if (nvram_get_int("dmz_enable") && nvram_get_int("dmz_ra"))
