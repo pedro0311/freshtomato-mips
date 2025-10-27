2091ec7b48617d92b84ac448cdd5d686213aa6d6
pedro <pedro311@gmail.com>
2025-10-27 11:34:39 +0100

    rc: snmp.c: fix commit 4131e84

--- release/src/router/rc/snmp.c
+++ release/src/router/rc/snmp.c
@@ -38,16 +38,16 @@ void start_snmp(void)
 		ro = nvram_safe_get("snmp_ro");
 
 		fprintf(fp, "agentaddress udp:%d\n"
-		            "syslocation %s\n"
-		            "syscontact %s <%s>\n"
-		            "rocommunity %s\n"
+		            "sysLocation %s\n"
+		            "sysContact %s\n"
 		            "sysName %s\n"
 		            "sysDescr %s\n"
+		            "rocommunity %s\n"
 		            "extend device /bin/echo \"%s\"\n"
 		            "extend version /bin/echo \"FreshTomato %s\"\n",
 		            nvram_get_int("snmp_port"),
 		            (location && *location ? location : "router"),
-		            (contact && *contact ? contact : "admin@freshtomato"), (contact && *contact ? contact : "admin@freshtomato"),
+		            (contact && *contact ? contact : "admin@freshtomato"),
 		            (name && *name ? name : "FreshTomato"),
 		            (descr && *descr ? descr : "router1"),
 		            (ro && *ro ? ro : "rocommunity"),
