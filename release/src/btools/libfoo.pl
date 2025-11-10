c0c8eb7e609293d4cc47db24e1c83c3770b91e4d
pedro <pedro311@gmail.com>
2025-11-10 20:50:29 +0100

    sqlite: update to 3.51.0

--- release/src/btools/libfoo.pl
+++ release/src/btools/libfoo.pl
@@ -208,7 +208,7 @@ sub fixDyn
 	fixDynDep("minidlna", "libid3tag.so.0.16.3");
 	fixDynDep("minidlna", "libexif.so.12");
 	fixDynDep("minidlna", "libFLAC.so.14.0.0");
-	fixDynDep("minidlna", "libsqlite3.so.3.50.4");
+	fixDynDep("minidlna", "libsqlite3.so.3.51.0");
 	fixDynDep("mysql", "libz.so.1");
 	fixDynDep("mysqld", "libz.so.1");
 	fixDynDep("mysqldump", "libz.so.1");
@@ -264,54 +264,54 @@ sub fixDyn
 
 	fixDynDep("nginx", "libpcre.so.1.2.13");
 	fixDynDep("nginx", "libpcreposix.so.0.0.7");
-	fixDynDep("nginx", "libpcre2-8.so.0.14.0");
-	fixDynDep("nginx", "libpcre2-posix.so.3.0.6");
+	fixDynDep("nginx", "libpcre2-8.so.0.15.0");
+	fixDynDep("nginx", "libpcre2-posix.so.3.0.7");
 	fixDynDep("nginx", "libwolfssl.so.42.2.0");
 	if ($is_arm ne "y") { # MIPS
 		fixDynDep("php-cgi", "libxml2.so.2.13.7");
 	}
 	else {
-		fixDynDep("php-cgi", "libxml2.so.16.1.0");
+		fixDynDep("php-cgi", "libxml2.so.16.1.1");
 	}
 	fixDynDep("php-cgi", "libpng16.so.16.50.0");
 	fixDynDep("php-cgi", "libiconv.so.2.7.0");
-	fixDynDep("php-cgi", "libsqlite3.so.3.50.4");
+	fixDynDep("php-cgi", "libsqlite3.so.3.51.0");
 	fixDynDep("php-cgi", "libcurl.so.4.8.0");
 	fixDynDep("php-cgi", "libjpeg.so.8.3.2");
 	fixDynDep("php-cgi", "libpcre.so.1.2.13");
 	fixDynDep("php-cgi", "libpcreposix.so.0.0.7");
-	fixDynDep("php-cgi", "libpcre2-8.so.0.14.0");
-	fixDynDep("php-cgi", "libpcre2-posix.so.3.0.6");
+	fixDynDep("php-cgi", "libpcre2-8.so.0.15.0");
+	fixDynDep("php-cgi", "libpcre2-posix.so.3.0.7");
 	fixDynDep("php-cgi", "libzip.so.5.5");
 	if ($is_arm ne "y") { # MIPS
 		fixDynDep("php-cli", "libxml2.so.2.13.7");
 	}
 	else {
-		fixDynDep("php-cli", "libxml2.so.16.1.0");
+		fixDynDep("php-cli", "libxml2.so.16.1.1");
 	}
 	fixDynDep("php-cli", "libpng16.so.16.50.0");
 	fixDynDep("php-cli", "libiconv.so.2.7.0");
-	fixDynDep("php-cli", "libsqlite3.so.3.50.4");
+	fixDynDep("php-cli", "libsqlite3.so.3.51.0");
 	fixDynDep("php-cli", "libcurl.so.4.8.0");
 	fixDynDep("php-cli", "libjpeg.so.8.3.2");
 	fixDynDep("php-cli", "libpcre.so.1.2.13");
 	fixDynDep("php-cli", "libpcreposix.so.0.0.7");
-	fixDynDep("php-cli", "libpcre2-8.so.0.14.0");
-	fixDynDep("php-cli", "libpcre2-posix.so.3.0.6");
+	fixDynDep("php-cli", "libpcre2-8.so.0.15.0");
+	fixDynDep("php-cli", "libpcre2-posix.so.3.0.7");
 	fixDynDep("php-cli", "libzip.so.5.5");
 	if ($is_arm ne "y") { # MIPS
 		fixDynDep("php-fpm", "libxml2.so.2.13.7");
 	}
 	else {
-		fixDynDep("php-fpm", "libxml2.so.16.1.0");
+		fixDynDep("php-fpm", "libxml2.so.16.1.1");
 	}
 	fixDynDep("php-fpm", "libpng16.so.16.50.0");
 	fixDynDep("php-fpm", "libiconv.so.2.7.0");
-	fixDynDep("php-fpm", "libsqlite3.so.3.50.4");
+	fixDynDep("php-fpm", "libsqlite3.so.3.51.0");
 	fixDynDep("php-fpm", "libcurl.so.4.8.0");
 	fixDynDep("php-fpm", "libjpeg.so.8.3.2");
-	fixDynDep("php-fpm", "libpcre2-8.so.0.14.0");
-	fixDynDep("php-fpm", "libpcre2-posix.so.3.0.6");
+	fixDynDep("php-fpm", "libpcre2-8.so.0.15.0");
+	fixDynDep("php-fpm", "libpcre2-posix.so.3.0.7");
 	fixDynDep("php-fpm", "libzip.so.5.5");
 
 	fixDynDep("curl", "libcurl.so.4.8.0");
@@ -386,8 +386,8 @@ sub fixDyn
 
 	fixDynDep("libipset.so.11.1.0", "libmnl.so.0.2.0");
 	fixDynDep("libglib-2.0.so.0.7400.7", "libiconv.so.2.7.0");
-	fixDynDep("libglib-2.0.so.0.7400.7", "libpcre2-8.so.0.14.0");
-	fixDynDep("libglib-2.0.so.0.7400.7", "libpcre2-posix.so.3.0.6");
+	fixDynDep("libglib-2.0.so.0.7400.7", "libpcre2-8.so.0.15.0");
+	fixDynDep("libglib-2.0.so.0.7400.7", "libpcre2-posix.so.3.0.7");
 
 	fixDynDep("benchmark", "libwolfssl.so.42.2.0");
 	fixDynDep("libmssl.so", "libwolfssl.so.42.2.0");
@@ -711,11 +711,11 @@ if ($is_arm ne "y") { # MIPS
 	genSO("${root}/usr/lib/libxml2.so.2.13.7", "${router}/libxml2/staged/usr/lib/libxml2.a", "${stripshared}", "-L${router}/zlib");
 }
 else {
-	genSO("${root}/usr/lib/libxml2.so.16.1.0", "${router}/libxml2/staged/usr/lib/libxml2.a", "${stripshared}", "-L${router}/zlib");
+	genSO("${root}/usr/lib/libxml2.so.16.1.1", "${router}/libxml2/staged/usr/lib/libxml2.a", "${stripshared}", "-L${router}/zlib");
 }
 genSO("${root}/usr/lib/libpcre.so.1.2.13", "${router}/pcre/.libs/libpcre.a", "${stripshared}");
-genSO("${root}/usr/lib/libpcre2-8.so.0.14.0", "${router}/pcre2/staged/usr/lib/libpcre2-8.a", "${stripshared}");
-#genSO("${root}/usr/lib/libpcre2-posix.so.3.0.6", "${router}/pcre2/staged/usr/lib/libpcre2-posix.a", "${stripshared}"); # (deleted)
+genSO("${root}/usr/lib/libpcre2-8.so.0.15.0", "${router}/pcre2/staged/usr/lib/libpcre2-8.a", "${stripshared}");
+#genSO("${root}/usr/lib/libpcre2-posix.so.3.0.7", "${router}/pcre2/staged/usr/lib/libpcre2-posix.a", "${stripshared}"); # (deleted)
 genSO("${root}/usr/lib/libiperf.so.0.0.0", "${router}/iperf/src/.libs/libiperf.a", "${stripshared}");
 genSO("${root}/usr/lib/libncurses.so.6", "${router}/libncurses/lib/libncurses.a", "${stripshared}");
 
