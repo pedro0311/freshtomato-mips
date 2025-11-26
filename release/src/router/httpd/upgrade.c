cd7b417b2a081586071e19f60b97944b2c5b1b65
pedro <pedro311@gmail.com>
2025-11-26 13:53:14 +0100

    Revert "httpd: add os_version[_last] to nvram and display it on every nvram call in asp scripts"
    This reverts commit 46f8a880fd6d83b31a4fd72a9a8a65c82c33611c.
    
    delete a mistakenly added commit

--- release/src/router/httpd/upgrade.c
+++ release/src/router/httpd/upgrade.c
@@ -33,9 +33,6 @@ void prepare_upgrade(void)
 			break;
 	}
 
-	nvram_set("os_version_last", tomato_shortver);
-	nvram_commit();
-
 	unlink("/var/log/messages");
 	unlink("/var/log/messages.0");
 	sync();
