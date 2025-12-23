28d8a7fc5c1375f1bf406640e01595819f713699
rs232 <46318040+wellloaded@users.noreply.github.com>
2025-12-23 14:59:38 +0200

    Update DNS status message formatting
    Shortened output with visual improvement:
    
    DNS	Failover order: (1) Stubby ➜ (2) 1.1.1.1:53 ➜ (3) 9.9.9.9:53

--- release/src/router/www/status-data.jsx
+++ release/src/router/www/status-data.jsx
@@ -77,7 +77,7 @@ if (nvram.dnscrypt_proxy == 1 || nvram.stubby_proxy == 1) {
 		if ((nvram.dnscrypt_proxy == 1 && nvram.dnscrypt_priority == 2) || (nvram.stubby_proxy == 1 && nvram.stubby_priority == 2)) /* no-resolv */
 			stats.dns[i] = 'Only: '+dns[i];
 		else if ((nvram.dnscrypt_proxy == 1 && nvram.dnscrypt_priority == 1) || (nvram.stubby_proxy == 1 && nvram.stubby_priority == 1)) /* strict-order */
-			stats.dns[i] = 'In order of (failover) preference: '+dns[i]+(stats.dns[i] > '' ? ','+stats.dns[i] : '');
+			stats.dns[i] = 'Failover order: (1) '+dns[i]+(stats.dns[i].length > 0 ? ' ➜ '+stats.dns[i].map(function(dns, idx) { return '(' + (idx+2) + ') ' + dns; }).join(' ➜ ') : '');
 		else
 			stats.dns[i] = 'Use all: '+dns[i]+(stats.dns[i] > '' ? ','+stats.dns[i] : '');
 	}
