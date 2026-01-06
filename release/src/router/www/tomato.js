24fc3cfb1f66caf90a609d1ad3b20f69fcec7c5f
rs232 <46318040+wellloaded@users.noreply.github.com>
2026-01-06 14:57:35 +0200

    GUI - tomato.js - update to Wiklink and added alternative text for the links

--- release/src/router/www/tomato.js
+++ release/src/router/www/tomato.js
@@ -3012,7 +3012,7 @@ function wikiLink() {
 	else
 		page = 'status-overview';
 
-	var res = '<a href="'+url+'/'+page+'" target="_blank" rel="noopener noreferrer">Wiki</a> | <a onclick="toggleTheme()" href="#">◐</a>';
+	var res = '<a href="'+url+'/'+page+'" target="_blank" rel="noopener noreferrer" title="Freshtomato Wiki">🇼</a> | <a onclick="toggleTheme()" href="#" title="Light/Dark mode">◐</a>';
 
 	W(res);
 	var alt = cookie.get('gui_themet');
