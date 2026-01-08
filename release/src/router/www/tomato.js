41a7e9e9ed620665934226d917c6a0239cb7ba93
pedro <pedro311@gmail.com>
2026-01-08 13:13:41 +0100

    Revert "GUI - tomato.js - update to Wiklink and added alternative text for the links"
    This reverts commit 24fc3cfb1f66caf90a609d1ad3b20f69fcec7c5f.
    
    Issues with older browsers

--- release/src/router/www/tomato.js
+++ release/src/router/www/tomato.js
@@ -3012,7 +3012,7 @@ function wikiLink() {
 	else
 		page = 'status-overview';
 
-	var res = '<a href="'+url+'/'+page+'" target="_blank" rel="noopener noreferrer" title="Freshtomato Wiki">🇼</a> | <a onclick="toggleTheme()" href="#" title="Light/Dark mode">◐</a>';
+	var res = '<a href="'+url+'/'+page+'" target="_blank" rel="noopener noreferrer">Wiki</a> | <a onclick="toggleTheme()" href="#">◐</a>';
 
 	W(res);
 	var alt = cookie.get('gui_themet');
