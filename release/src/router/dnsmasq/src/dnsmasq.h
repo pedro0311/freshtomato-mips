cc647d9611529b26986bcb4328ff0b7f6b783dd5
pedro <pedro311@gmail.com>
2026-08-05 08:37:25 +0200

    dnsmasq: update to v2.93-1f12252

--- release/src/router/dnsmasq/src/dnsmasq.h
+++ release/src/router/dnsmasq/src/dnsmasq.h
@@ -723,7 +723,7 @@ struct resolvc {
 #endif
 };
 
-/* adn-hosts parms from command-line (also dhcp-hostsfile and dhcp-optsfile and dhcp-hostsdir*/
+/* addn-hosts parms from command-line (also dhcp-hostsfile and dhcp-optsfile and dhcp-hostsdir*/
 #define AH_DIR      1
 #define AH_INACTIVE 2
 #define AH_WD_DONE  4
@@ -1171,7 +1171,7 @@ struct dhcp_relay {
 };
 
 extern struct daemon {
-  /* datastuctures representing the command-line and 
+  /* datastructures representing the command-line and 
      config file arguments. All set (including defaults)
      in option.c */
 
