85b27d0478c4e77772e837caacb9b294d90724d7
pedro <pedro311@gmail.com>
2026-08-31 14:24:38 +0200

    dnsmasq: update to v2.93-cf9c9c7

--- release/src/router/dnsmasq/src/dnsmasq.c
+++ release/src/router/dnsmasq/src/dnsmasq.c
@@ -2021,9 +2021,7 @@ static void do_tcp_connection(struct listener *listener, time_t now, int slot)
   struct in_addr netmask;
   int pipefd[2];
   struct iovec tcpbuff;
-#ifdef HAVE_LINUX_NETWORK
   unsigned char a = 0;
-#endif
   netmask.s_addr = 0;
   
   while ((confd = accept(listener->tcpfd, NULL, NULL)) == -1 && errno == EINTR);
@@ -2149,7 +2147,7 @@ static void do_tcp_connection(struct listener *listener, time_t now, int slot)
 	     
 	     To avoid the race, the parent blocks here until a 
 	     single byte comes back up the pipe, which
-	     is sent by the child has finshed the close. */
+	     is sent when the child has finished the close. */
 	  read_write(pipefd[0], &a, 1, RW_READ);
 	  
 	  daemon->tcp_pids[slot] = p;
@@ -2258,9 +2256,7 @@ int swap_to_tcp(struct frec *forward, time_t now, int status, struct dns_header 
     {
       pid_t p;
       int i, pipefd[2];
-#ifdef HAVE_LINUX_NETWORK
       unsigned char a = 0;
-#endif
       
       /* check to see if we have a free tcp process slot. */
       for (i = daemon->max_procs - 1; i >= 0; i--)
@@ -2291,7 +2287,7 @@ int swap_to_tcp(struct frec *forward, time_t now, int status, struct dns_header 
 	     
 	     To avoid the race, the parent blocks here until a 
 	     single byte comes back up the pipe, which
-	     is sent by the child has finshed the close. */
+	     is sent when the child has finished the close. */
 	  read_write(pipefd[0], &a, 1, RW_READ);
 	  
 	  /* i holds index of free slot */
--- release/src/router/dnsmasq/src/option.c
+++ release/src/router/dnsmasq/src/option.c
@@ -745,7 +745,7 @@ static int numeric_check(char *a)
 {
   char *p;
 
-  if (!a)
+  if (!a || *a == 0)
     return 0;
 
   unhide_metas(a);
@@ -771,11 +771,14 @@ static int strtoul_check(char *a, u32 *res)
   
   if (!numeric_check(a))
     return 0;
+  
   x = strtoul(a, NULL, 10);
-  if (errno || x > UINT32_MAX) {
-    errno = 0;
-    return 0;
-  }
+  if (errno || x > UINT32_MAX)
+    {
+      errno = 0;
+      return 0;
+    }
+  
   *res = (u32)x;
   return 1;
 }
@@ -1688,7 +1691,7 @@ static int parse_dhcp_opt(char *errstr, char *arg, int flags)
 	  parse_hex(comma, new->val, digs, (flags & DHOPT_MATCH) ? &new->u.wildcard_mask : NULL, NULL);
 	  new->flags |= DHOPT_HEX;
 	}
-      else if (is_dec)
+      else if (is_dec && !(opt_len & OT_DHCP6_VENDOR))
 	{
 	  int i, val = atoi(comma);
 	  /* assume numeric arg is 1 byte except for
@@ -1896,7 +1899,7 @@ static int parse_dhcp_opt(char *errstr, char *arg, int flags)
 	    {
 	      /* First arg is Enterprise ID (4 bytes)
 		 subsequent are length fields (2 bytes each) + string */
-	      int i, commas = 1;
+	      int enterprise, i, commas = 1;
 	      unsigned char *p, *newp;
 
 	      for (i = 0; comma[i]; i++)
@@ -1908,14 +1911,11 @@ static int parse_dhcp_opt(char *errstr, char *arg, int flags)
 	      arg = comma;
 	      comma = split(arg);
 
-	      if (arg && *arg)
-	      {
-	      unsigned int enterprise = atoi(arg);
-	      PUTLONG(enterprise, p);
-	      }
+	      if (atoi_check(arg, &enterprise))
+		PUTLONG(enterprise, p);
 	      else
-	      goto_err(_("missing enterprise ID in dhcp-option"));
-
+		goto_err(_("bad or missing enterprise ID in dhcp-option"));
+		  
 	      arg = comma;
 	      comma = split(arg);
 
